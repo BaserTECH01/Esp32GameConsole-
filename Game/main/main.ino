8ea137d45bb13682127dc18ecdf2decfa5cd4ebf
unrealengineertut <89449572+unrealengineertut@users.noreply.github.com>
2022-05-16 00:15:51 +0300

    .

--- Game/main/main.ino
+++ Game/main/main.ino
@@ -13,16 +13,20 @@ int8_t SquareXSize = screenXSize / screenDXSize;
 
 void setup()
 {
-  Serial.begin(9600);
+    Serial.begin(9600);
     tft.init();
     tft.setRotation(2);
     tft.fillScreen(TFT_BLACK);
 }
 
+int direction = 1;
+double oldSeconds = 0;
+double allSeconds = 0;
+
 void loop()
 {
-    unsigned long runMicros = millis();
     double allSeconds = (double)millis()/1000;
+    double deltaSeconds = allSeconds - oldSeconds;
 
-    Serial.println(allSeconds);
+    Serial.println(deltaSeconds);
 }
