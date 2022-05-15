01144419321b2b9a96b216715e103375274aec01
BaserTECH01 <77685916+BaserTECH01@users.noreply.github.com>
2022-05-16 01:34:20 +0300

    git gel

--- Game/main/main.ino
+++ Game/main/main.ino
@@ -11,6 +11,12 @@ int8_t screenDXSize = 20;
 int8_t SquareYSize = screenYSize / screenDYSize;
 int8_t SquareXSize = screenXSize / screenDXSize;
 
+
+uint32_t last;
+int y=0;
+boolean direction=1;
+
+
 void setup()
 {
     Serial.begin(9600);
@@ -19,12 +25,12 @@ void setup()
     tft.fillScreen(TFT_BLACK);
 }
 
-int direction = 1;
+
 float oldSeconds = 0;
 float allSeconds = 0;
 
 float konum = 0;
-float speed = 12.0f;
+float speed = 80.0f;
 
 void loop()
 {
@@ -32,11 +38,24 @@ void loop()
     float deltaSeconds = allSeconds - oldSeconds;
 
     Serial.println(deltaSeconds);
+   if (millis() - last > 3000) {
+    direction=!direction;
+    last = millis();
+   }
+    
+   if(direction==1){
+      konum = konum + (speed * deltaSeconds);
+     tft.fillRect(120, (int)konum, 25, 25, TFT_GREEN);
+     tft.fillRect(120, (int)konum-25,25, 25,TFT_BLACK);      
+      y=konum;
+    }
+  
+   if(direction==0){
+      konum = konum - (speed * deltaSeconds);
+     tft.fillRect(120, (int)konum, 25, 25, TFT_GREEN);
+     tft.fillRect(120, (int)konum+25,25, 25,TFT_BLACK);      
+      y=konum;
+    }
 
-    konum = konum + (speed * deltaSeconds);
-
-    tft.fillScreen(TFT_BLACK);
-    tft.fillRect(120, (int)konum, 25, 25, TFT_GREEN);
-
-    oldSeconds = allSeconds;
+       oldSeconds = allSeconds;
 }
